15c02e3fd2fe4e1dd1119b383701e4ae556df684
Ajgorek04 <igorblendzinski12@gmail.com>
2024-02-07 23:03:02 +0100

    adding button component

--- src/app/components/Nav/Nav.jsx
+++ src/app/components/Nav/Nav.jsx
@@ -3,18 +3,21 @@ import { UserInfo } from "../UserInfo/UserInfo";
 import { NavItems } from "../NavItems/NavItems";
 import CLOSE from "../../../../public/Close.svg";
 import Image from "next/image";
+import { Button } from "../Button/Button";
 
 export function Nav({ isNavVisible, toggleNavVisibility }) {
   return (
     <nav className={`${styles.nav} ${isNavVisible ? styles.active : ""}`}>
       <UserInfo />
       <NavItems />
-      <Image
-        src={CLOSE}
-        alt="closenav"
-        className={styles.close}
-        onClick={toggleNavVisibility}
-      />
+      <Button>
+        <Image
+          src={CLOSE}
+          alt="closenav"
+          className={styles.close}
+          onClick={toggleNavVisibility}
+        />
+      </Button>
     </nav>
   );
 }
