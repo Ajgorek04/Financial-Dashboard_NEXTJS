79f99f4c5e543f2b8806cda64982097d5ac33f6d
Ajgorek04 <igorblendzinski12@gmail.com>
2024-02-15 22:25:56 +0100

    repairing view of the nav in the mobile

---
+++ src/app/components/AddNote/AddNote.jsx
@@ -0,0 +1,5 @@
+import styles from "./AddNote.module.css";
+
+export function AddNote() {
+  return <div className={styles.addNote}>ADD NOTE</div>;
+}
--- src/app/components/Notes/Notes.jsx
+++ src/app/components/Notes/Notes.jsx
@@ -1,16 +1,22 @@
+"use client";
 import styles from "./Notes.module.css";
 import { FinancialBox } from "../FinancialBox/FinancialBox";
+import { useState } from "react";
+import { AddNote } from "../AddNote/AddNote";
 
 export function Notes() {
+  const [isShownNote, setIsShownNote] = useState(false);
+
   return (
     <div className={styles.notes}>
       <FinancialBox>
         <div className={styles.addNote}>
           <h3>Notes</h3>
-          <button>ADD NOTE</button>
+          <button onClick={() => setIsShownNote(true)}>ADD NOTE</button>
         </div>
         <hr />
       </FinancialBox>
+      {isShownNote && <AddNote />}
     </div>
   );
 }
