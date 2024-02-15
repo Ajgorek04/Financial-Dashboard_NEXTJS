c19eb9c91efbe7c4b207509385d8981e0367d4f1
Ajgorek04 <igorblendzinski12@gmail.com>
2024-02-15 22:53:46 +0100

    adding buttons

--- src/app/components/AddNote/AddNote.jsx
+++ src/app/components/AddNote/AddNote.jsx
@@ -1,5 +1,18 @@
 import styles from "./AddNote.module.css";
 
 export function AddNote() {
-  return <div className={styles.addNote}>ADD NOTE</div>;
+  return (
+    <div className={styles.addNote}>
+      <h1>NEW NOTE</h1>
+
+      <div className={styles.noteContent}>
+        <p>TITLE</p>
+        <input type="text" />
+        <p>CONTENT</p>
+        <textarea cols="30" rows="10"></textarea>
+        <button className={styles.addButton}>ADD NOTE</button>
+        <button className={styles.cancelButton}>CANCEL</button>
+      </div>
+    </div>
+  );
 }
